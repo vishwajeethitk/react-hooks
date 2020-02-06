9596697b1d94c977d7019f82b2e0a76a94f06b00
viswajeetk <viswajeetk@INviswajeetkumars-MacBook-Pro.local>
2020-02-06 12:55:51 +0530

    No issues

--- pages/ImageChangeOnScroll.js
+++ pages/ImageChangeOnScroll.js
@@ -5,7 +5,7 @@ const ImageChangeOnScroll = () => {
     return (
         <div>
             {
-                ['bill-gates'/*, 'mark-zuckerberg', 'martin-fowler', 'steve-jobs'*/].map(speaker => {
+                ['bill-gates', 'mark-zuckerberg', 'martin-fowler', 'steve-jobs'].map(speaker => {
                     return (
                         <div key={speaker}>
                             <ImageToggleOnScroll primaryImg={'/speakers/bw/' + speaker + '-bw.jpg'}
